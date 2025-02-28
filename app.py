21b11e10487b737f0714865205a43332e97d8d14
Anvesh <anveshreddy.bemore@example.com>
2025-02-28 02:10:43 -0500

    update yaml

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase app with credentials
-cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+cred = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 firebase_admin.initialize_app(cred) 
 
 # Initialize Firestore DB
